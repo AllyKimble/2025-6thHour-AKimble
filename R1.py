fa3a97c228466ee153c6ab62aab079fd6b0b5ebc
Ally Kimble <ak126472@student.wpsok.org>
2026-03-23 13:48:52 -0500

    ADDED R1

---
+++ R1.py
@@ -0,0 +1,58 @@
+#Name: Ally Kimble
+#Class: 6th Hour
+#Assignment: HW-R1
+
+#1. Print "Hello World!"
+
+print("Hello World!")
+
+#2. Create three variables with strings as values.
+
+a = "Oh"
+b = "Uh"
+c = "Ah"
+
+#3. Create three variables with booleans as values.
+
+Oh = "True"
+Uh = "False"
+Ah = "True"
+
+#4. Create three variables with integers as values.
+
+d = 6
+e = 7
+f = 67
+
+#5. Create a variable that asks for the user's name and prints it.
+
+name = input("What is your name? ")
+print("Hello " + name + "!")
+
+#6. Add together the three variables from #4 and print the result.
+
+sum = d + e + f
+print(sum)
+
+#7. Subtract the second variable from the first from #4 and print the result.
+
+sub = d - e
+print(sub)
+
+#8. Multiply the third variable from #4 to the result of #6 and print the result.
+
+mult = f * sum
+print(mult)
+
+#9. Create a variable that asks the user for a number with a decimal (a float).
+
+decimal = float(input("Input a decimal number: "))
+
+#10. Round the variable from #9 to the nearest whole number and print the result.
+
+print(round(decimal))
+
+#11. Find the remainder (hint: modulo) of 355/113. Print the result.
+
+div = 355/113
+print(div)
