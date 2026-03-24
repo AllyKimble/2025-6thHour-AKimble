41710c5e44ce8d0867bcd8f64eb6940ed2824682
Ally Kimble <ak126472@student.wpsok.org>
2026-03-24 13:37:33 -0500

    ADDED R3

---
+++ R3.py
@@ -0,0 +1,37 @@
+#Name: Ally Kimble
+#Class: 6th Hour
+#Assignment: HW-R3
+
+
+#1. import random and print "Hello World!"
+
+import random
+
+print("Hello World!")
+
+#2. Create three variables that each randomly generate an integer between 1 and 10, print each number on the same line.
+
+a = random.randint(1, 10)
+b = random.randint(1, 10)
+c = random.randint(1, 10)
+
+print(a, b, c)
+
+#3. Create a list containing 5 strings listing 5 colors.
+
+color_list = ["red", "blue", "green", "yellow", "pink"]
+
+#4. Use a function to randomly choose one of the 5 colors from the list and print the result.
+
+print(random.choice(color_list))
+
+#5. Create an if statement that determines which of the three variables from #2 is the lowest.
+
+if a < b and c:
+    print(a, "is the smallest.")
+elif b < a and c:
+    print(b, "is the smallest.")
+elif c < a and b:
+    print(c, "is the smallest.")
+elif a == b == c:
+    print("They are all the same number stupid :P")
